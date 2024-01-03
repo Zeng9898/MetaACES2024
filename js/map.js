2bd98beab29f02774478467a533cc73bba66c972
ZengYihong <beliefloatsky@gmail.com>
2024-01-03 10:08:50 +0800

    add cozzi alfar hotel

--- js/map.js
+++ js/map.js
@@ -2,26 +2,26 @@ let map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 24.96488759106457, lng: 121.21720159878147 },
+        center: { lat: 24.970340288657315, lng: 121.19574962361749 },
         zoom: 14,
     });
 
     var markers = [
-        {
-            position: { lat: 24.962546792366922, lng: 121.22714965375683 },
-            title: "Hub Hotel",
-            content: '<div style="font-weight: bold; color: red;">Hub Hotel</div>',
-        },
+        // {
+        //     position: { lat: 24.962546792366922, lng: 121.22714965375683 },
+        //     title: "Hub Hotel",
+        //     content: '<div style="font-weight: bold; color: red;">Hub Hotel</div>',
+        // },
         {
             position: { lat: 24.957121932497376, lng: 121.20436360019136 },
             title: "Hotel IN",
             content: '<div style="font-weight: bold; color: red;">Hotel IN</div>',
         },
-        {
-            position: { lat: 24.957301075161634, lng: 121.20720460609108 },
-            title: "Hotel-J-Taoyuan",
-            content: '<div style="font-weight: bold; color: red;">Hotel-J-Taoyuan</div>',
-        },
+        // {
+        //     position: { lat: 24.957301075161634, lng: 121.20720460609108 },
+        //     title: "Hotel-J-Taoyuan",
+        //     content: '<div style="font-weight: bold; color: red;">Hotel-J-Taoyuan</div>',
+        // },
         {
             position: { lat: 24.964896010833193, lng: 121.21715087359995 },
             title: "Hotel Kuva Chateau",
@@ -48,10 +48,21 @@ function initMap() {
             content: '<div style="font-weight: bold; color: red;">Hotel MU</div>',
         },
         {
+            position: { lat: 25.02117297444458, lng: 121.21887084784531 },
+            title: "Alfar Hotel",
+            content: '<div style="font-weight: bold; color: red;">Alfar Hotel</div>',
+        },
+        { 
+            position: { lat: 25.018097488409236, lng: 121.21376408445516 },
+            title: "HOTEL COZZI",
+            content: '<div style="font-weight: bold; color: red;">HOTEL COZZI</div>',
+        },
+        {
             position: { lat: 24.970340288657315, lng: 121.19574962361749 },
             title: "National Central University",
-            content: '<div style="font-weight: bold; color: red;">National Central University</div>',
-        }
+            content: '<div style="font-weight: bold; color: red;">National Central Universit</div>',
+        },
+        
         // Add more markers as needed
     ];
     markers.forEach(function (markerInfo) {
